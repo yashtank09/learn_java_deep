9993709fae8d171da5afda3188c060182f84c109
Yash Tank <yashtank09@gmail.com>
2021-06-04 20:38:14 +0530

    Unicode standard & how bits stores

---
+++ CheckBits.java
@@ -0,0 +1,11 @@
+// This is how int stores 32bits we are converting the integeral value.
+
+public class CheckBits {
+    public static void main(String[] args) {
+        int x = -5;
+        int a = 5;
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(Integer.toBinaryString(a));
+
+    }
+}
