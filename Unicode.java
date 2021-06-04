1ef4b6284dcb3d0c1239aeccbe8dbb4f4aa3d64b
Yash Tank <yashtank09@gmail.com>
2021-06-04 21:42:39 +0530

    Unicode devangiri from unicode.org

--- Unicode.java
+++ Unicode.java
@@ -5,7 +5,7 @@ public class Unicode {
         char x = 0x0370;
         System.out.println(x);
 
-        // Here we are printing devangiri from unicode.or
+        // Here we are printing devangiri from unicode.org
         for(char c=0x0900;c<=0x0970;c++ ){
             System.out.print(c+" ");
         }
