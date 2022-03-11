3e2d9be97ee22fe8b67e4d970e29d1108016ab99
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2022-03-11 17:38:10 +0530

    Format string using System.out.printf("%-15s%03d%n")

---
+++ test2.java
@@ -0,0 +1,13 @@
+public class test2 {
+    public static void main(String[] args) {
+        int p = 0;
+        int n = 10;
+        for(int i = 1; p<n; i++){
+            System.out.println("value P befor: " + p);
+            System.out.println("value I: " + i);
+            p = p + i;
+            System.out.println("value P now: " + p +"\n");
+        }
+
+    }
+}
---
+++ tests.java
@@ -0,0 +1,29 @@
+public class tests{
+    public static void main(String[] args) {
+        // // with string we can not directly do meth operators it's recognized as concadination operator
+        // System.out.println("Sum of 2 values "+ 4 + 5 +" <- This understands as concadination operator and print as string");
+        // // if we want to do that heres is the solution.
+        // int a = 12/3;
+        // System.out.println("SUM->" + (4+5));
+        // System.out.println("Division->" + a);
+        // System.out.println(4+5);
+        int i = 5-4;
+        switch(i) {
+            case 0:
+                System.out.println("Zero!!!!");
+                break;
+            case 1:
+                System.out.println("One!!!");
+                break;
+            default:
+                try {
+                    System.out.println("Your wrong bro...");
+                } catch (Exception e) {
+                    //TODO: handle exception
+                    System.out.println(68+1);
+                    System.out.println("That's Wrong number....");
+                }
+                break;
+        }
+    }
+}
