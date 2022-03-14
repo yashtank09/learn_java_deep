b5e4f4a52b7d0f25e90409fe338cfd7c79d7fa65
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2022-03-14 14:03:23 +0530

    Solved challange but it's to confusing in input and output

---
+++ FileHandelling.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class FileHandelling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int count = 0;
+        // String a = scn.nextLine();
+        // while(a != null){
+        //     System.out.println(count++ + " " + a);
+        //     a = scn.nextLine();
+        // }
+        // scn.close();
+        
+        int i=1;
+        while (sc.hasNext()) {
+            String s=sc.nextLine();
+            System.out.println(i + " " + s);
+            i++;
+        }
+
+        /*
+            This will print every live after hiting enter and the challange was the same and I was confused about that.
+        */
+        // int count = 0;
+        // while(scn.hasNext()){
+        //     String s = scn.nextLine();
+        //     System.out.print(++count + " " + s);   
+        // }
+        // scn.close();
+        
+    }
+}
