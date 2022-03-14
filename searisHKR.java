456fbb516156cffe766579487fe9c0b991b8184d
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2022-03-14 12:39:25 +0530

    Solve a challange of HackerRank that takes an input and get filter their types

---
+++ searisHKR.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class searisHKR {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+                int result = 0;
+                int a = in.nextInt();
+                int b = in.nextInt();
+                int n = in.nextInt();
+
+                for (int j = 0; j < n; j++ ){
+                    if(j==0){
+                        result = result + (a + (int)Math.pow(2,j) * b);
+                    }
+                    else{
+                        result = result + ((int)Math.pow(2,j) * b);
+                    }
+                    System.out.print(result+" ");
+                }
+                System.out.println();
+            }
+        in.close();
+    }
+}
