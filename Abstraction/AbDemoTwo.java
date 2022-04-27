a41304d09a9c7826cc3da0d1ba9d7d4db832aaa1
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2022-04-27 10:10:54 +0530

    One more example of abstraction

---
+++ Abstraction/AbDemoTwo.java
@@ -0,0 +1,32 @@
+package Abstraction;
+
+// 
+public class AbDemoTwo {
+    public static void main(String[] args) {
+        Iphone obj = new Iphone();
+        Samsung obj1 = new Samsung();
+        show(obj);
+    }
+    
+    // we can also instansiate class object as parameter
+    public static void show(Phone obj){
+        obj.showConfig();
+    }
+    
+}
+
+abstract class Phone {
+    public abstract void showConfig();
+}
+
+class Iphone extends Phone{
+    public void showConfig(){
+        System.out.println("2 GB, IOS 15");
+    }
+}
+
+class Samsung extends Phone{
+    public void showConfig(){
+        System.out.println("2 GB, Android 12");
+    }
+}
