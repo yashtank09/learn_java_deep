dd0021e6d8dad17d3ecbf6c0ad8cd2ab9ee059f2
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2022-08-03 19:17:00 +0530

    Loop Session over

---
+++ LoopChallanges2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * LoopChallanges2
+ */
+public class LoopChallanges2 {
+
+    public static void main(String[] args) {
+        int numb, rem, i=0;
+        Scanner sc = new Scanner(System.in);
+        numb = sc.nextInt();
+
+        while(numb>0){
+            rem = numb % 10;
+            System.out.println(rem);
+            numb = numb/10;
+            i++;
+        }
+        System.out.println("\nCount of digit in given number: "+i);
+    }
+}
--- fibonaccee.java
+++ fibonaccee.java
@@ -1,21 +1,35 @@
+import java.util.Scanner;
+
 /**
  * fibonaccee
  */
 public class fibonaccee {
 
     public static void main(String[] args) {
-        
+
         Scanner user_input = new Scanner(System.in);
-        int fib1 = 0, fib2 = 1, i, n;
-        n = user_input.nextInt();
+        /*
+         * int fib1 = 0, fib2 = 1, i, n;
+         * n = user_input.nextInt();
+         * 
+         * System.out.print(fib1 + " ");
+         * for (i = 1; i <= n; i++) {
+         * System.out.print(fib2 + " ");
+         * int next = fib1 + fib2;
+         * 
+         * fib1 = fib2;
+         * fib2 = next;
+         * }
+         */
+        int a = 0, b = 1, c, n = 10;
 
-        System.out.print(fib1+" ");
-        for(i=1;i<=n;i++){
-            System.out.print(fib2 + " ");
-            int next = fib1 + fib2;
-
-            fib1 = fib2;
-            fib2 = next;
+        System.out.print(a + ",");
+        for (int i = 0; i < n; i++) {
+            System.out.print(b + ",");
+            c = a + b;
+            a = b;
+            b = c;
         }
+
     }
 }
