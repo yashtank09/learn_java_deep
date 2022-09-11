a6ba5340c41058e1aecfa79ead5c7c533ddc1427
Yash Tank <62990171+yashtank09@users.noreply.github.com>
2022-09-11 21:43:08 +0530

    commented remaining contents

--- OOPJ/Inheritance/OverridingExam.java
+++ OOPJ/Inheritance/OverridingExam.java
@@ -1,5 +1,9 @@
 package Inheritance;
-
+/* 
+    Polymorphism:
+        - Overriding is used for runtime polymorphism.
+        - Overriding will only achive by Inheritance.
+ */
 class T1{
     protected void display(){
         System.out.println("Super T1 Display.");
@@ -15,6 +19,11 @@ class T2 extends T1{
 public class OverridingExam {
     public static void main(String[] args) {
         T1 a = new T2();
+        /*
+            - A method call depends on the object and new means object will be created on runtime.
+            - Compiler only generate bytecode, will not decide which method call.
+            - It has will be decided on the runtime that which method calls on runtime.
+        */
         a.display();
     }
 }
